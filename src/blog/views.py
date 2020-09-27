a6432795a1c14895ad7951b50b6942e483921fbb
Feras Maosud <feras.masoud1998@gmail.com>
2020-09-27 08:44:02 +0300

    Add Notations in index.html

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,37 @@
 from django.http import request
 from django.shortcuts import render
 
+posts = [{
+    'titale': 'التدوينة 1',
+    'contant': 'نص تجريبي',
+    'post_date': '15-3-2020',
+    'author': 'احمد'
+}, {
+    'titale': 'التدوينة 2',
+    'contant': 'نص تجريبي',
+    'post_date': '30-4-2020',
+    'author': 'فراس'
+}, {
+    'titale': 'التدوينة 3',
+    'contant': 'نص تجريبي',
+    'post_date': '3-3-2015',
+    'author': 'عمر'
+}, {
+    'titale': 'التدوينة 4',
+    'contant': 'نص تجريبي',
+    'post_date': '15-9-2016',
+    'author': 'محمد'
+}, {
+    'titale': 'التدوينة 5',
+    'contant': 'نص تجريبي',
+    'post_date': '1-1-2020',
+    'author': 'عبد'
+}]
+
+
 def home(request):
-    return render(request, 'blog/index.html')
+    context = {
+        'title': 'الصفحة الرئيسية',
+        'posts': posts
+    }
+    return render(request, 'blog/index.html', context)
