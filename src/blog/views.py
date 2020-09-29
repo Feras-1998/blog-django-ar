d055f3807c3322843f579eaea748c5d85eb80783
Feras Maosud <feras.masoud1998@gmail.com>
2020-09-29 17:22:04 +0300

    Add detail.html

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,34 +1,6 @@
 from django.http import request
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-
-# posts = [{
-#     'titale': 'التدوينة 1',
-#     'contant': 'نص تجريبي',
-#     'post_date': '15-3-2020',
-#     'author': 'احمد'
-# }, {
-#     'titale': 'التدوينة 2',
-#     'contant': 'نص تجريبي',
-#     'post_date': '30-4-2020',
-#     'author': 'فراس'
-# }, {
-#     'titale': 'التدوينة 3',
-#     'contant': 'نص تجريبي',
-#     'post_date': '3-3-2015',
-#     'author': 'عمر'
-# }, {
-#     'titale': 'التدوينة 4',
-#     'contant': 'نص تجريبي',
-#     'post_date': '15-9-2016',
-#     'author': 'محمد'
-# }, {
-#     'titale': 'التدوينة 5',
-#     'contant': 'نص تجريبي',
-#     'post_date': '1-1-2020',
-#     'author': 'عبد'
-# }]
-
 
 def home(request):
     context = {
@@ -40,3 +12,12 @@ def home(request):
 def about(request):
     context = {'title': 'من أنا'}
     return render(request, 'blog/about.html', context)
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {
+        'title': post.title,
+        'post': post,
+    }
+
+    return render(request, 'blog/detail.html', context)
