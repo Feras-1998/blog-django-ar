7b01948d523431f50b6fb1f2b574de17774da56a
Feras Maosud <feras.masoud1998@gmail.com>
2020-10-02 21:49:55 +0300

    Fix Some Problem in side Bar using Custom Tags

--- src/blog/views.py
+++ src/blog/views.py
@@ -20,15 +20,6 @@ def post_detail(request, post_id):
     
     post = get_object_or_404(Post, pk=post_id)
     comments = post.comments.filter(active=True)
-    comment_form = NewComment()
-    new_comment = None
-    
-    context = {
-        'title': post.title,
-        'post': post,
-        'comments': comments,
-        'comment_form': comment_form,
-    }
 
     if request.method == 'POST':
         comment_form = NewComment(data=request.POST)
@@ -37,7 +28,14 @@ def post_detail(request, post_id):
             new_comment.post = post
             new_comment.save()
             comment_form = NewComment()
-        else:
-            comment_form = NewComment()
+    else:
+        comment_form = NewComment()
+    
+    context = {
+        'title': post.title,
+        'post': post,
+        'comments': comments,
+        'comment_form': comment_form,
+    }
 
     return render(request, 'blog/detail.html', context)
