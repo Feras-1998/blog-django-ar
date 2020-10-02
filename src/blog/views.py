2f8d707bb6ee85f18233b5413e2f764ffbb751f6
Feras Maosud <feras.masoud1998@gmail.com>
2020-10-02 04:34:23 +0300

    Connect Comment models To DataBase

--- src/blog/views.py
+++ src/blog/views.py
@@ -10,19 +10,34 @@ def home(request):
     }
     return render(request, 'blog/index.html', context)
 
+
 def about(request):
     context = {'title': 'من أنا'}
     return render(request, 'blog/about.html', context)
 
+
 def post_detail(request, post_id):
+    
     post = get_object_or_404(Post, pk=post_id)
     comments = post.comments.filter(active=True)
     comment_form = NewComment()
+    new_comment = None
+    
     context = {
         'title': post.title,
         'post': post,
         'comments': comments,
         'comment_form': comment_form,
     }
+
+    if request.method == 'POST':
+        comment_form = NewComment(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            comment_form = NewComment()
+        else:
+            comment_form = NewComment()
 
     return render(request, 'blog/detail.html', context)
